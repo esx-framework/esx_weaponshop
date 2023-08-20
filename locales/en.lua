7c806d2307152aefe28375867cdf72ff3abc7f36
YOMAN1792 <94007829+YOMAN1792@users.noreply.github.com>
2023-08-20 10:18:13 -0400

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -12,6 +12,5 @@ Locales ['en'] = {
   ['already_owned'] = 'you already own this weapon!',
   ['shop_menu_title'] = 'ammu-Nation',
   ['shop_menu_prompt'] = 'press [E] to access the Ammu-Nation.',
-  ['shop_menu_item'] = '%s USD',
   ['map_blip'] = 'Ammu-Nation',
 }
